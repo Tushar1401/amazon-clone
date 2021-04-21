1b51fe84512073c064d807d8fa766740e9eb7716
Tushar1401 <tushararora1401@gmail.com>
2021-04-21 13:29:44 +0530

    Updates

---
+++ precache-manifest.6036b5c3f97100f83fde605925b497d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6601e770a59e0c6352494db2ec8d4ae9",
+    "url": "/amazon-clone/index.html"
+  },
+  {
+    "revision": "949ed8cce7db367811f2",
+    "url": "/amazon-clone/static/css/main.40d66b32.chunk.css"
+  },
+  {
+    "revision": "24d75e9b989f2ca70ef4",
+    "url": "/amazon-clone/static/js/2.be8c7780.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/amazon-clone/static/js/2.be8c7780.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "949ed8cce7db367811f2",
+    "url": "/amazon-clone/static/js/main.5493017e.chunk.js"
+  },
+  {
+    "revision": "3101b3b2c4581d0f9087",
+    "url": "/amazon-clone/static/js/runtime-main.1cf4aa72.js"
+  }
+]);
